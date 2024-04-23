60ee19c999c38a27202dec7d919bae224ff35a88
AlexZacharkiw <alexanderzacharkiw@dawsoncollege.qc.ca>
2024-04-23 09:32:22 -0400

    add backend skeleton and basic local object

---
+++ library-app-server/data/catalogue.js
@@ -0,0 +1,21 @@
+const catalogue = {
+    articles: [
+        { headline : 'Very important news',
+        description : 'Here is the description of a very important news story' },  
+        { headline : 'Not Very important news',
+        description : 'Here is the description of a not so very important news story' },   
+    ],
+    books: [
+        { 
+            name : 'A book',
+            pages : 25,
+            cover : hard
+        }
+    ],
+    papers: [
+        {
+            name : 'study',
+            progress : 'complete'
+        }
+    ]
+};

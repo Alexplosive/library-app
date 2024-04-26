4542a1f84d61b8ff1192f652ec81677dc18c18fe
AlexZacharkiw <alexanderzacharkiw@dawsoncollege.qc.ca>
2024-04-26 13:58:10 -0400

    fix spacing issue with navbar

--- library-app-client/src/views/Navbar.jsx
+++ library-app-client/src/views/Navbar.jsx
@@ -1,11 +1,11 @@
 function Navbar({ setScreen, setUser, user }) {
   if(user){
     return(
-      <div className="h-max m-10">
+      <div className="h-max m-5">
         <header className="">
           <h1 onClick={ () => setUser(undefined) } className="text-2xl hover:bg-purple-800 bg-purple-600 cursor-pointer font-bold border border-current p-3 m-0 text-center rounded-3xl">Current User: {user.username} ({user.role}) / Logout</h1>
         </header>
-        <nav className="flex justify-items-center gap-0 m-10">
+        <nav className="flex justify-items-center gap-0 m-5">
           <h2 onClick={() => {setScreen('BOOKS') }} className="text-2xl font-bold bg-purple-600 rounded-3xl hover:bg-purple-800 flex-1 border border-current p-3 m-0 text-center cursor-pointer">Books</h2>
           <h2 onClick={() => {setScreen('ARTICLES') }} className="text-2xl font-bold bg-purple-600 rounded-3xl hover:bg-purple-800 flex-1 border border-current p-3 m-0 text-center cursor-pointer">Articles</h2>
           <h2 onClick={() => {setScreen('PAPERS') }} className="text-2xl font-bold bg-purple-600 rounded-3xl hover:bg-purple-800 flex-1 border border-current p-3 m-0 text-center cursor-pointer">Papers</h2>
